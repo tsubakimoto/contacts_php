ea17b377b3dfe971f60bdb352d60d36aa762a309
Yuta Matsumura <dev.tsubaki@gmail.com>
2013-05-06 13:17:43 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'config.php';
+require_once 'functions.php';
+
+?>
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>ホーム：お問い合わせフォーム</title>
+</head>
+<body>
+	<h1>お問い合わせフォーム</h1>
+</body>
+</html>
